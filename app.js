07d1ebd5e4e0055a6e2748bdae2f94907dd40f72
joao victor <thehoodgamer20@gmail.com>
2022-09-01 20:54:13 -0300

    adicionado mais frases

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 function frase(){
 
-    const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!", "Até o diabo tem medo de você agora!", "Desativa essa guia anônima aí que eu to vendo kkkkk", "E esse nuds aí no teu celular hein? kkkk apaga saporrakkkkk", "Calma garotão, essa vontade é temporária, o orgulho de ganhar é eterno!", "Prohibido el modo sexo... Primer aviso!"]
+    const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!", "Até o diabo tem medo de você agora!", "Desativa essa guia anônima aí que eu to vendo kkkkk", "E esse nuds aí no teu celular hein? kkkk apaga saporrakkkkk", "Calma garotão, essa vontade é temporária, o orgulho de ganhar é eterno!", "Prohibido el modo sexo... Primer aviso!", "Quem perder o No Fap será banido do Grupo."]
+    console.log(array.length)
     let entrada = document.getElementById("entrada").value
     let frase = Math.floor(Math.random()*7)
     alertify.alert(entrada+" dias",array[frase], 
