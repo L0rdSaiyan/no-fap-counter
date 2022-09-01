a6d46af03aea35b222c1782cb3cf9dd88d96f1e5
joao victor <thehoodgamer20@gmail.com>
2022-09-01 20:49:36 -0300

    estilizado o campo e o botão do formulário

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 function frase(){
 
-    const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!", "Até o diabo tem medo de você agora!", "Desativa essa guia anônima aí que eu to vendo kkkkk", "E esse nuds aí no teu celular hein? kkkk apaga saporrakkkkk", "Calma garotão, essa vontade é temporária, o orgulho de ganhar é eterno!", "pro"]
+    const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!", "Até o diabo tem medo de você agora!", "Desativa essa guia anônima aí que eu to vendo kkkkk", "E esse nuds aí no teu celular hein? kkkk apaga saporrakkkkk", "Calma garotão, essa vontade é temporária, o orgulho de ganhar é eterno!", "Prohibido el modo sexo... Primer aviso!"]
     let entrada = document.getElementById("entrada").value
     let frase = Math.floor(Math.random()*7)
     alertify.alert(entrada+" dias",array[frase], 
