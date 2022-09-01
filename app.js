d6f139826daa6d859f9373c58d798f9693bc7141
joao victor <thehoodgamer20@gmail.com>
2022-09-01 20:38:27 -0300

    adicionado mais frases ao array

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 function frase(){
 
-    const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!"]
+    const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!", "Até o diabo tem medo de você agora!", "Desativa essa guia anônima aí que eu to vendo kkkkk", "E esse nuds aí no teu celular hein? kkkk apaga saporrakkkkk", "Calma garotão, essa vontade é temporária, o orgulho de ganhar é eterno!", "pro"]
     let entrada = document.getElementById("entrada").value
-    let frase = Math.floor(Math.random()*2)
+    let frase = Math.floor(Math.random()*7)
     alertify.alert(entrada+" dias",array[frase], 
     function(){
-        alertify.sucess("LEMBRE-SE: COM 30 DIAS VOCÊ SE TORNA UMA LENDA")
+        alertify.message("LEMBRE-SE: COM 30 DIAS VOCÊ SE TORNA UMA LENDA")
     });
 
 }
