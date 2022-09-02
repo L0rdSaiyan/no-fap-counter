43c095a20b6cfc997b98ef1cf737b67a6308aa01
joao victor <thehoodgamer20@gmail.com>
2022-09-01 21:07:37 -0300

    identado o código e adicionado estruturas condicionais para validar os dias

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+function frase() {
 
-function frase(){
-
+    //Array contendo as frases que serão apresentadas no Alertify.
     const array = ["Continue firme, soldado!", "O seu pau pode estar mole, mas a sua honra está intacta.", "NÃO PEÇA O LINK! NÃO SE CORROMPA POR ESSA PRAGA!", "O MAIOORRRR!!!", "Até o diabo tem medo de você agora!", "Desativa essa guia anônima aí que eu to vendo kkkkk", "E esse nuds aí no teu celular hein? kkkk apaga saporrakkkkk", "Calma garotão, essa vontade é temporária, o orgulho de ganhar é eterno!", "Prohibido el modo sexo... Primer aviso!", "Quem perder o No Fap será banido do Grupo."]
-    console.log(array.length)
+    //Variável que recebe a quantidade de dias informada pelo usuário.
     let entrada = document.getElementById("entrada").value
-    let frase = Math.floor(Math.random()*7)
-    alertify.alert(entrada+" dias",array[frase], 
-    function(){
-        alertify.message("LEMBRE-SE: COM 30 DIAS VOCÊ SE TORNA UMA LENDA")
-    });
+    //Variável que recebe um número aleatório para ser o indice do Array para apresentar a frase aleatoriamente.
+    let frase = Math.floor(Math.random() * 10)
+    
 
+    //Estruturas condicionais para validar os dias.
+    if (entrada.length == 0) {
+
+        alertify.error("Insira a quantidade de dias!")
+
+    } else if (entrada < 0) {
+
+        alertify.error("Valores negativos são inválidos!")
+
+    }
+
+    else {
+
+        alertify.alert(entrada + " dias", array[frase],
+            function () {
+                alertify.message("LEMBRE-SE: COM 30 DIAS VOCÊ SE TORNA UMA LENDA")
+            });
+    }
 }
 
 
